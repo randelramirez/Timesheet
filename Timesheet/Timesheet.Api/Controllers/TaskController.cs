d75be9cde942025001213b8150337436f3c663ef
Randel Ramirez <randelramirez1@gmail.com>
2020-06-26 16:09:34 +0800

    Clean up

--- Timesheet/Timesheet.Api/Controllers/TaskController.cs
+++ Timesheet/Timesheet.Api/Controllers/TaskController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Timesheet.Api.Services;
-using Timesheet.Api.ViewModels;
 using Timesheet.Api.ViewModels.Extensions;
 
 namespace Timesheet.Api.Controllers
