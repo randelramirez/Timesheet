d1ed4aa2a2abb20d5346ef21f29cbcbf2ca85358
Randel Ramirez <randelramirez1@gmail.com>
2020-08-07 21:26:05 +0800

    Clean up using expression bodied members

--- Timesheet/Timesheet.Api/ViewModels/Extensions/TimecardViewModelHelper.cs
+++ Timesheet/Timesheet.Api/ViewModels/Extensions/TimecardViewModelHelper.cs
@@ -5,35 +5,28 @@ namespace Timesheet.Api.ViewModels.Extensions
 {
     public static class TimecardViewModelHelper
     {
-        public static TimecardViewModel ToViewModel(this Core.Timecard model)
-        {
-            return new TimecardViewModel
+        public static TimecardViewModel ToViewModel(this Core.Timecard model) =>
+            new TimecardViewModel
             {
                 Id = model.Id,
                 Date = model.Date,
                 Hours = model.Hours,
                 TaskId = model.TaskId
             };
-        }
-
-        public static CreateTimecardViewModel ToCreateViewModel(this Core.Timecard model)
-        {
-            return new CreateTimecardViewModel
+       
+        public static CreateTimecardViewModel ToCreateViewModel(this Core.Timecard model) =>
+        
+            new CreateTimecardViewModel
             {
                 Date = model.Date,
                 Hours = model.Hours,
                 TaskId = model.TaskId
             };
-        }
+        
+        public static IEnumerable<TimecardViewModel> ToViewModels(this IEnumerable<Core.Timecard> model) =>
+             model.Select(t => t.ToViewModel());
 
-        public static IEnumerable<TimecardViewModel> ToViewModels(this IEnumerable<Core.Timecard> model)
-        {
-            return model.Select(t => t.ToViewModel());
-        }
-
-        public static IEnumerable<CreateTimecardViewModel> ToCreateViewModels(this IEnumerable<Core.Timecard> model)
-        {
-            return model.Select(t => t.ToCreateViewModel());
-        }
+        public static IEnumerable<CreateTimecardViewModel> ToCreateViewModels(this IEnumerable<Core.Timecard> model) =>
+             model.Select(t => t.ToCreateViewModel());   
     }
 }
--- Timesheet/Timesheet.Api/ViewModels/Extensions/ViewModelToEntity.cs
+++ Timesheet/Timesheet.Api/ViewModels/Extensions/ViewModelToEntity.cs
@@ -8,29 +8,20 @@ namespace Timesheet.Api.ViewModels.Extensions
 {
     public static class ViewModelToEntity
     {
-        public static Timecard ToTimecardEntity(this TimecardViewModel viewModel)
-        {
-            return new Timecard() { Id = viewModel.Id, Date = viewModel.Date, Hours = viewModel.Hours, TaskId = viewModel.TaskId };
-        }
+        public static Timecard ToTimecardEntity(this TimecardViewModel viewModel) =>
+             new Timecard() { Id = viewModel.Id, Date = viewModel.Date, Hours = viewModel.Hours, TaskId = viewModel.TaskId };
 
-        public static Timecard ToTimecardEntity(this CreateTimecardViewModel viewModel)
-        {
-            return new Timecard() { Date = viewModel.Date, Hours = viewModel.Hours, TaskId = viewModel.TaskId };
-        }
+        public static Timecard ToTimecardEntity(this CreateTimecardViewModel viewModel) =>
+             new Timecard() { Date = viewModel.Date, Hours = viewModel.Hours, TaskId = viewModel.TaskId };
+        
+        public static IEnumerable<Timecard> ToTimecardEntities(this IEnumerable<TimecardViewModel> viewModels) =>
+             viewModels.Select(t => t.ToTimecardEntity());
 
-        public static IEnumerable<Timecard> ToTimecardEntities(this IEnumerable<TimecardViewModel> viewModels)
-        {
-            return viewModels.Select(t => t.ToTimecardEntity());
-        }
+        public static IEnumerable<Timecard> ToTimecardEntities(this IEnumerable<CreateTimecardViewModel> viewModels) =>
+             viewModels.Select(t => t.ToTimecardEntity());
+        
+        public static Core.Task ToTaskEntity(this TaskViewModel viewModel) =>
+             new Core.Task() { Id = viewModel.Id, Name = viewModel.Name };
 
-        public static IEnumerable<Timecard> ToTimecardEntities(this IEnumerable<CreateTimecardViewModel> viewModels)
-        {
-            return viewModels.Select(t => t.ToTimecardEntity());
-        }
-
-        public static Core.Task ToTaskEntity(this TaskViewModel viewModel)
-        {
-            return new Core.Task() { Id = viewModel.Id, Name = viewModel.Name };
-        }
     }
 }
