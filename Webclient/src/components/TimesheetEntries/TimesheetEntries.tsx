77c5c1d17adfd93e95653cd94faf2f3e5c9fc9d5
Randel Ramirez <randelramirez1@gmail.com>
2021-09-05 15:40:19 +0800

    Add TimesheetEntries component

---
+++ Webclient/src/components/TimesheetEntries/TimesheetEntries.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { IEntry } from "../../containers/Timesheet/Timesheet";
+import AnimationContainer from "../../containers/AnimationContainer/AnimationContainer";
+import TimesheetEntry from "../TimesheetEntry/TimesheetEntry";
+
+export interface ITimesheetEntriesProps {
+  entries: IEntry[];
+  taskOptions: any;
+  onChange: any;
+  deleteHandler: any;
+}
+
+export default function TimesheetEntries({
+  entries,
+  taskOptions,
+  onChange,
+  deleteHandler,
+}: ITimesheetEntriesProps) {
+  const controls: JSX.Element[] = entries.map(
+    (entry: IEntry, index: number) => {
+      return (
+        <AnimationContainer key={entry.id}>
+          <TimesheetEntry
+            data={entry}
+            taskOptions={taskOptions || []}
+            onChange={onChange}
+            deleteHandler={deleteHandler}
+          />
+        </AnimationContainer>
+      );
+    }
+  );
+
+  // A component can only return 1 parent container, we use Fragrament to circumvent that
+  return <React.Fragment>{controls}</React.Fragment>;
+}
