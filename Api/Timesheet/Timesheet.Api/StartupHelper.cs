fba1dcf6b82f76281a432aeabaeefb6f8fa8cc88
Randel Ramirez <randelramirez1@gmail.com>
2021-09-06 14:52:54 +0800

    Add Startup Helper

---
+++ Api/Timesheet/Timesheet.Api/StartupHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Timesheet.Infrastructure.Persistence;
+
+namespace Timesheet.Api
+{
+    public static class StartupHelper
+    {
+        public static void SeedDataTimesheetContext(this IApplicationBuilder application)
+        {
+            using (var serviceScope = application.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<TimesheetContext>();
+
+                context.Database.EnsureCreated();
+
+                var databaseSeeder = new Infrastructure.Helpers.DataInitializer(context);
+                if (!context.Tasks.Any())
+                {
+                    databaseSeeder.SeedTasks();
+                }
+                if (!context.Timecards.Any())
+                {
+                    databaseSeeder.SeedTimesheets();
+                }
+            }
+        }
+    }
+}
