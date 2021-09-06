02feccd7d74152ac40daac665933b3e904710d2f
Randel Ramirez <randelramirez1@gmail.com>
2021-09-06 14:40:21 +0800

    Add Task and Timesheet Controllers

---
+++ Api/Timesheet/Timesheet.Api/Controllers/TaskController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+using Timesheet.Api.Services;
+using Timesheet.Api.ViewModels;
+using Timesheet.Api.ViewModels.Extensions;
+
+namespace Timesheet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskService service;
+
+        public TaskController(TaskService service)
+        {
+            this.service = service ?? throw new ArgumentNullException();
+        }
+
+        // GET: api/<TimesheetController>
+        [HttpGet]
+        [HttpHead]
+        public async Task<ActionResult<Core.Task[]>> Get()
+        {
+            var tasks = await this.service.GetAllAsync();
+            return Ok(tasks.ToViewModels());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Core.Task[]>> Get(int id)
+        {
+            var task = await this.service.GetAsync(id);
+            if(task != null)
+            {
+                return Ok(task);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Core.Task[]>> Post(Core.Task model)
+        {
+            model.Id = 0;
+           
+            await this.service.Add(model);
+            return CreatedAtRoute(nameof(TaskController.Get), new { id = model.Id });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Core.Task[]>> Put(Core.Task model)
+        {
+            model.Id = 0;
+
+            await this.service.Add(model);
+            return NoContent();
+        }
+
+        [HttpPatch]
+        public ActionResult<Core.Task[]> Patch(JsonPatchDocument<UpdateTaskViewModel> patchDocument)
+        {
+            //model.Id = 0;
+
+            //await this.service.Add(model);
+            //return CreatedAtRoute(nameof(TaskController.Get), new { id = model.Id });
+            var model = new UpdateTaskViewModel();
+            patchDocument.ApplyTo(model,ModelState);
+
+            if(!TryValidateModel(model))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            return NoContent();
+        }
+
+        public override ActionResult ValidationProblem([ActionResultObjectValue] ModelStateDictionary modelStateDictionary)
+        {
+            // So we don't need to write a logic, we get the same logic/code from Startup.cs ApiBehaviorOptions
+            var options = HttpContext.RequestServices.GetRequiredService<IOptions<ApiBehaviorOptions>>();
+            return (options.Value.InvalidModelStateResponseFactory(this.ControllerContext) as ActionResult);
+        }
+    }
+}
---
+++ Api/Timesheet/Timesheet.Api/Controllers/TimesheetController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Timesheet.Api.Helpers;
+using Timesheet.Api.Services;
+using Timesheet.Api.ViewModels;
+using Timesheet.Api.ViewModels.Extensions;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Timesheet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimesheetController : ControllerBase
+    {
+        private readonly LinkGenerator linkGenerator;
+        private readonly TimecardService service;
+
+        public TimesheetController(LinkGenerator linkGenerator, TimecardService service)
+        {
+            this.linkGenerator = linkGenerator ?? throw new ArgumentNullException(nameof(linkGenerator));
+            this.service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        // GET: api/<TimesheetController>
+
+        [HttpGet] // the actions are the actual endpoints
+        [HttpHead]
+        public async Task<ActionResult<Timesheet.Core.Timecard[]>> Get()
+        {
+            try
+            {
+                var timecards = await this.service.GetAllAsync();
+                return Ok(timecards.ToViewModels());
+            }
+            catch (Exception)
+            {
+                //requestResult = this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        // GET api/<TimesheetController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Core.Timecard>> Get(int id)
+        {
+            var timecard = await this.service.GetAsync(id);
+            if (await this.service.GetAsync(id) != null)
+            {
+                return Ok(timecard);
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("({ids})", Name = nameof(TimesheetController.GetTimecards))]
+        public async Task<ActionResult<Core.Timecard>> GetTimecards([FromRoute]
+        [ModelBinder(BinderType = typeof(CommaSeparatedModelBinder))] IEnumerable<int> ids)
+        {
+            if(ids == null)
+            {
+                return BadRequest();
+            }
+
+            var timecards = await this.service.GetAllAsync(ids);
+
+            if(ids.Count() != timecards.Count())
+            {
+                // We return all, of if any is missing, we return not found
+                return NotFound();
+            }
+
+            return Ok(timecards);
+        }
+
+        // POST api/<TimesheetController>
+        [HttpPost]
+        public async Task<ActionResult<Core.Timecard>> Post([FromBody] IEnumerable<CreateTimecardViewModel> timecards)
+        {
+            var timecardModels = timecards.ToTimecardEntities().ToList();
+            //var url = this.linkGenerator.GetPathByAction(HttpContext, "Get", values: new { timecards = timecardModels });
+            //await this.service.Add(timecardModels);
+            //return Created(url, timecardModels);
+            await this.service.Add(timecardModels);
+            var idsAsString = string.Join(",", timecardModels.ToViewModels().Select(t => t.Id));
+            return CreatedAtRoute(nameof(TimesheetController.GetTimecards),new { ids = idsAsString }, timecardModels.ToViewModels());
+        }
+
+        // PUT api/<TimesheetController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TimesheetController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpOptions]
+        public IActionResult GetTimesheetOptions()
+        {
+            Response.Headers.Add("Allow", string.Join(',',"GET", "OPTIONS", "POST","DELETE"));
+            
+            // provide response body(not covered by http standard)
+            return Ok();
+        }
+    }
+}
