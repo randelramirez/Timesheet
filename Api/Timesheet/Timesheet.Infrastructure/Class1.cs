e36f261b00142b08fe15981a19802668935b4c95
Randel Ramirez <randelramirez1@gmail.com>
2021-09-06 14:42:32 +0800

    Add DataContext

--- Api/Timesheet/Timesheet.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Timesheet.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ Api/Timesheet/Timesheet.Infrastructure/Persistence/TimesheetContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Timesheet.Core;
+
+namespace Timesheet.Infrastructure.Persistence
+{
+    public class TimesheetContext : DbContext
+    {
+        public DbSet<Timecard> Timecards { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public TimesheetContext(DbContextOptions<TimesheetContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Timecard>().HasKey(tc => tc.Id);
+
+            modelBuilder.Entity<Task>().HasKey(t => t.Id);
+
+            // Timecard and Task Relationship
+            modelBuilder.Entity<Timecard>().HasOne(t => t.Task)
+                .WithMany(t => t.Timecards)
+                .HasForeignKey(tc => tc.TaskId);
+
+        }
+    }
+}
